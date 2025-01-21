12dfd1affad22287332dafd6791d37ce49ca40e4
Andy <andywickoren@gmail.com>
2025-01-20 20:45:39 -0600

    update main.jsx to use hashrouter for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import App from "../src/components/App/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
